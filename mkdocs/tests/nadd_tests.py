b2a2acb01cbd7a86c8994a586e29e7c308a348a5
Daethe <daethe.pro@gmail.com>
2017-07-16 16:40:00 +0000

    Rename add_tests to nadd_tests du to failure when run before gh-deploy

--- mkdocs/tests/nadd_tests.py
+++ mkdocs/tests/nadd_tests.py
@@ -2,11 +2,13 @@
 # coding: utf-8
 
 from __future__ import unicode_literals
-from os import chdir, mkdir, rmdir
+
 import io
 import unittest
+from datetime import date, datetime
+import os
 import tempfile
-from datetime import datetime, date
+import yaml
 
 from mkdocs.commands import add, new
 
@@ -16,52 +18,70 @@ class AddTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         tempdir = tempfile.mkdtemp()
-        chdir(tempdir)
-        new.new("testproject", None)
-        chdir("testproject")
+        os.chdir(tempdir)
+        new.new("myproject", None)
+        os.chdir("myproject")
+
+        # Setup config for test
+        io.open('_template/test.md', 'w').write(u"{{filename}} created at {{datetime}}")
+        yaml_conf = yaml.dump({'test': {
+            'filename': {'type': 'filename'},
+            'datetime': {'type': 'datetime', 'options': {'format': '%A %B %Y at %X'}}
+        }})
+        io.open('_template/_config.yml', 'a').write(unicode(yaml_conf))
+
+    def test_add(self):
+        expect = """testadd created at {0}""".format((datetime.now()).strftime('%A %B %Y at %X'))
+        add.add('test', './', 'testadd')
+        self.assertEqual(io.open('docs/testadd.md', 'r').read(), expect)
 
     # Is current working directory a mkdocs project
     def test_is_cwd_mkdocs_project(self):
-        """Current directory is a mkdocs project"""
         self.assertTrue(add._is_cwd_mkdocs_project())
 
     # Is current working directory as a template directory
     def test_is_cwd_as_template_directory(self):
-        """Current directory as a template directory or not"""
         self.assertTrue(add._is_cwd_as_template_directory('_template'))
         self.assertFalse(add._is_cwd_as_template_directory('dir_error'))
 
     # As template in template directory
     def test_as_template_in_template_directory(self):
-        """As or not the template file in directory"""
         self.assertTrue(add._as_template_in_template_directory('_template/sample.md'))
-        self.assertFalse(add._as_template_in_template_directory('_template/test.md'))
+        self.assertFalse(add._as_template_in_template_directory('_template/testfalse.md'))
 
     # As config in template directory
     def test_as_config_in_template_directory(self):
-        """As the config file in directory"""
         self.assertTrue(add._as_config_in_template_directory('_template'))
         self.assertFalse(add._as_config_in_template_directory('_test'))
+
+    # As not file in docs directory
+    def test_as_not_file_in_docs_directory(self):
         self.assertFalse(add._as_not_file_in_docs_directory('docs/index.md'))
         self.assertTrue(add._as_not_file_in_docs_directory('no_file_in_dir'))
 
     # As output directory
     def test_as_output_directory(self):
-        """Output directory already exist"""
-        mkdir('docs/output')
+        os.mkdir('docs/output')
         self.assertTrue(add._as_output_directory('docs/output', False))
-        rmdir('docs/output')
+        os.rmdir('docs/output')
         self.assertFalse(add._as_output_directory('output', False))
         self.assertTrue(add._as_output_directory('docs/output', True))
 
     # Get config file
-    def test_get_config_file_with_undefined_name(self):
-        """Undefined name, not in config file"""
-        self.assertEqual(add._get_config_file('_template', 'undefined'), None)
+    def test_get_config_file(self):
+        expect = {
+            'filename': {'type': 'filename'},
+            'datetime': {'type': 'datetime'},
+            'datetime_format': {'type': 'datetime', 'options': {'format': '%A %B %Y at %X'}},
+            'date': {'type': 'date'},
+            'date_format': {'type': 'date', 'options': {'format': '%x'}},
+            'text': {'type': 'text', 'value': 'Lorem ipsum dolor sit amet'}
+        }
+        self.assertEqual(add._get_config_file('_template', 'sample'), expect)
+        self.assertIsNone(add._get_config_file('_template', 'undefined'))
 
     # Check existance
     def test_check_existance(self):
-        """Everything is ok and exist"""
         self.assertTrue(add._check_existance('_template',
                                              '_template/sample.md',
                                              './',
@@ -74,37 +94,21 @@ class AddTests(unittest.TestCase):
                                               True))
 
     # Write file
-    @unittest.skip("Need to do some search")
     def test_write_file(self):
-        """File value is template parsed value"""
         config = {
             'filename': {'type': 'filename'},
-            'datetime': {'type': 'datetime'},
-            'datetime_format': {'type': 'datetime', 'options': {'format': '%A %B %Y at %X'}},
-            'date': {'type': 'date'},
-            'date_format': {'type': 'date', 'options': {'format': '%A %B %Y'}},
-            'text': {'type': 'text', 'value': 'Lorem ipsum dolor sit amet'}
+            'datetime': {'type': 'datetime', 'options': {'format': '%A %B %Y at %X'}}
         }
-        add._write_file('_template/sample.md',
+        add._write_file('_template/test.md',
                         'docs/testsample.md',
                         config,
                         'testsample')
 
-        expect = format("""The filename: testsample
-        The datetime: %s
-        The formatted datetime: %s
-        The date: %s
-        The formatted date: %s
-        The text: Lorem ipsum dolor sit amet""",
-                        (datetime.now()).strftime('%c'),
-                        (datetime.now()).strftime('%A %B %Y at %X'),
-                        (date.today()).strftime('%x'),
-                        (date.today()).strftime('%A %B %Y'))
+        expect = """testsample created at {0}""".format((datetime.now()).strftime('%A %B %Y at %X'))
         self.assertEqual(expect, io.open('docs/testsample.md', 'r').read())
 
     # Parse keyword
     def test_parse_keyword(self):
-        """Parse variable"""
         config = {'type': 'date'}
         self.assertEqual(add._parse_keyword(config, None), (date.today().strftime('%x')))
 
